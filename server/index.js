1747b30b4e44f58eac7522477f2efa33beb89f39
[Lisa Miner] <midnightmoet@gmail.com>
2022-02-21 17:19:09 -0500

    changes to client

--- server/index.js
+++ server/index.js
@@ -11,8 +11,8 @@ const app = express();
 // JSON body parser
 app.use(express.json());
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(5000, () => {
+    console.log("Server started on port 5000");
 });
 
 // Set up routers
