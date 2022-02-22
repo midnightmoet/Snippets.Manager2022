c37c0daa224f8c44dccc19496274dd4692c20edf
[Lisa Miner] <midnightmoet@gmail.com>
2022-02-22 10:56:16 -0500

    changes to index

--- client/src/index.js
+++ client/src/index.js
@@ -2,21 +2,25 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
+	return (
+		<div>
+			<Hello />
+			<Clock />
+		</div>
+	);
+}
+
+function Clock() {
 	const [time, setTime] = useState(new Date().toLocaleTimeString());
 	
     useEffect(() => {
         setInterval(() => setTime(new Date().toLocaleTimeString()), 1000);
     }, []);
-
-	return (
-		<div>
-			<Clock time={time} />
-		</div>
-	);
+	return <p>It is currently {time}</p>;
 }
 
-function Clock(props) {
-	return <p>It's currently {props.time}</p>;
+function Hello(){
+	return <h1>Hello world!</h1>
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
