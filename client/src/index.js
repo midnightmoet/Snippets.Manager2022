ba15a7e9cf768e6cd8219b88878ee426ab1ad35c
[Lisa Miner] <midnightmoet@gmail.com>
2022-02-28 07:22:35 -0500

    another step in the course

--- client/src/index.js
+++ client/src/index.js
@@ -2,25 +2,54 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 function App() {
+
+	const [messageShown, setMessageShown] = useState(false);
+
+	// Generic update 
+	useEffect(() => {
+		console.log("Component updating...");
+	});
+
+
+// Specific update
+	useEffect(() => {
+		console.log("Specific state update!");
+	}, [messageShown])
+
 	return (
-		<div>
-			<Hello />
-			<Clock />
-		</div>
+		<>
+			
+			<button onClick={() => setMessageShown(!messageShown)}>
+				Toggle Message
+			</button>
+			{messageShown &&( 
+				<>
+					<Random />
+					<p>Some message</p>
+				</>
+			)}
+		</>
 	);
 }
 
-function Clock() {
-	const [time, setTime] = useState(new Date().toLocaleTimeString());
-	
-    useEffect(() => {
-        setInterval(() => setTime(new Date().toLocaleTimeString()), 1000);
-    }, []);
-	return <p>It is currently {time}</p>;
-}
+function Random(){
+	const [randomNumber, setRandomNumber] = useState(Math.random());
 
-function Hello(){
-	return <h1>Hello world!</h1>
+	useEffect(() => {
+		console.log("Component mount!");
+		const intervalId = setInterval(() => {
+			setRandomNumber(Math.random());
+			console.log("Setting new random number...");
+		}, 1000);
+
+		// cleanup function
+		return () => {
+			console.log("Component unmount!");
+			clearInterval(intervalId);
+		};
+	}, []);
+
+	return <h1>{randomNumber}</h1>;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
