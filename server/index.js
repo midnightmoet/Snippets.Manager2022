7704b5b809731300f2c34fb87629a970c08f4c69
[Lisa Miner] <midnightmoet@gmail.com>
2022-03-03 16:26:33 -0500

    pushing client to save where I am

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
@@ -10,9 +11,15 @@ const app = express();
 
 // JSON body parser
 app.use(express.json());
+// CORS is middleware that allows us to accept requests from other origins
+app.use(
+    cors({ 
+        origin: "http://localhost:3000", 
+    })
+);
 
 app.listen(5000, () => {
-    console.log("Server started on port 5000");
+	console.log("Server started on port 5000");
 });
 
 // Set up routers
@@ -21,11 +28,13 @@ app.use("/snippets", require("./routers/snippetRouter"));
 
 // connect to mongodb
 mongoose.connect(
-    process.env.MDB_CONNECT_STRING,
-{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}, (err) => {
-    if (err) return console.log(err);
-    console.log("Connected to Mongodb");
-});
+	process.env.MDB_CONNECT_STRING,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(err) => {
+		if (err) return console.log(err);
+		console.log("Connected to Mongodb");
+	}
+);
