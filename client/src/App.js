71ce6ce2055a58ab1beb62eacf66f5be6d922b35
[Lisa Miner] <midnightmoet@gmail.com>
2022-03-21 14:11:04 -0400

    adding the what ifs for users logging in or registering

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import Axios from 'axios';
 import React from 'react';
+import { UserContextProvider } from './context/UserContext';
 import Router from './Router';
 import "./style/index.scss";
 
@@ -8,8 +9,10 @@ Axios.defaults.withCredentials = true;
 export default function App() {
 
     return (
-        <div className="container">
-            <Router />
-        </div>
+        <UserContextProvider>
+            <div className="container">
+                <Router />
+            </div>
+        </UserContextProvider>
     );
 }
